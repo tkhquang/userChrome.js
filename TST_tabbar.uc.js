b9f3bd824065daae07960c897626365b36babb0d
tkhquang <37518707+tkhquang@users.noreply.github.com>
2018-05-07 21:02:01 +0700

    Create TST_tabbar.uc.js
    Auto hide tab bar only when Tree Style Tab sidebar is visible.

---
+++ TST_tabbar.uc.js
@@ -0,0 +1,15 @@
+//Auto hide tab bar only when Tree Style Tab sidebar is visible.
+
+(function() {
+    var tabbar = document.getElementById("TabsToolbar");
+    function showHideTabbar() {
+        var sidebarBox = document.getElementById("sidebar-box");
+        var sidebarTST = sidebarBox.getAttribute("sidebarcommand");
+        if (!sidebarBox.hidden && sidebarTST === "treestyletab_piro_sakura_ne_jp-sidebar-action") {
+            tabbar.style.visibility = "collapse";
+        }
+        else tabbar.style.visibility = "visible";
+    }
+    var observer = new MutationObserver(showHideTabbar);
+    observer.observe(document.getElementById("sidebar-box"), { attributes: true, attributeFilter: ["sidebarcommand", "hidden"] });
+})();
