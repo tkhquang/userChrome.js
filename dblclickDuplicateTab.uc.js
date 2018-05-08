e75d42258528d51a28d19a5ddc635f50fe89a99d
tkhquang <37518707+tkhquang@users.noreply.github.com>
2018-05-08 20:21:38 +0700

    Update dblclickDuplicateTab.uc.js

--- dblclickDuplicateTab.uc.js
+++ dblclickDuplicateTab.uc.js
@@ -1,10 +1,13 @@
-gBrowser.tabContainer.addEventListener("dblclick", function(event) {
-    if (event.target.localName == "tab" && event.button == 0) {
-        // 1.Duplicate tab then move to that new tab directly
+(function() {
+    gBrowser.tabContainer.addEventListener("dblclick", function(event) {
+        if (event.target.localName != "tab" || event.button != 0) {
+            return;
+        }
+        //Duplicate tab then move to that new tab directly
         gBrowser.selectedTab = gBrowser.duplicateTab(event.target);
 
-        // 2.Duplicate tab and load it in background
+        //Duplicate tab and load it in background
         //var dupTab = gBrowser.duplicateTab(event.target);
         //gBrowser.reloadTab(dupTab);
-    }
-});
+    });
+})();
