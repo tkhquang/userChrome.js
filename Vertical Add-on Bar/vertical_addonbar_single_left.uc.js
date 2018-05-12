c6ab6935b982b756b919669b1351550d291c7761
AleksT <37518707+tkhquang@users.noreply.github.com>
2018-05-12 15:54:54 +0700

    Left + Single column only

--- Vertical Add-on Bar/vertical_addonbar_single_left.uc.js
+++ Vertical Add-on Bar/vertical_addonbar_single_left.uc.js
@@ -106,6 +106,7 @@ overflow: scroll!important;
 #vertical-toolbar-toolbox scrollbar {
 display: none!important;
 }`;
+
     var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
     var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
     sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
