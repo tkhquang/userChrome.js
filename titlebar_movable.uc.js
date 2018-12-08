a91a02c1a7e6c549dbe1490d1e511758dfddde77
AleksT <37518707+tkhquang@users.noreply.github.com>
2018-12-08 19:57:57 +0700

    updated css

--- titlebar_movable.uc.js
+++ titlebar_movable.uc.js
@@ -3,93 +3,108 @@
 //https://imgur.com/sXZrBMb
 //https://imgur.com/a/GHvgjzu
 
-(function() {
-    if(location != 'chrome://browser/content/browser.xul') return;
+(function () {
+    if (location != "chrome://browser/content/browser.xul") {
+        return;
+    }
     try {
         CustomizableUI.createWidget({
-            id: 'pagetitle-bar',
-            type: 'custom',
+            id: "pagetitle-bar",
+            type: "custom",
             defaultArea: CustomizableUI.AREA_NAVBAR,
             onBuild: function(aDocument) {
-                var toolbaritem = aDocument.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'toolbaritem');
-                var image = aDocument.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'image');
-                image.setAttribute('src', 'chrome://branding/content/icon16.png');
-                image.id = 'pagetitle-bar-image';
-                var props = {
-                    id: 'pagetitle-bar',
-                    class: 'chromeclass-toolbar-additional',
-                    titlepage: document.getElementById('main-window').getAttribute('title'),
-                    //flex: '1',
-                    pack: 'center', //remove this line if you dont want the text to be centered by default
-                    align: 'center',
-                    label: 'Page Title Bar'
+                const toolbaritem = aDocument.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "toolbaritem");
+                const image = aDocument.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "image");
+                image.setAttribute("src", "chrome://branding/content/icon16.png");
+                image.id = "pagetitle-bar-image";
+                const props = {
+                    id: "pagetitle-bar",
+                    class: "chromeclass-toolbar-additional",
+                    titlepage: document.getElementById("main-window").getAttribute("title"),
+                    tooltiptext: document.getElementById("main-window").getAttribute("title"),
+                    pack: "center",
+                    align: "center",
+                    label: "Page Title Bar"
                 };
-                for (var p in props) {
-                    toolbaritem.setAttribute(p, props[p]);
-                }
+                Object.entries(props).forEach(
+                    ([key, value]) => toolbaritem.setAttribute(key, value)
+                );
                 toolbaritem.appendChild(image);
                 return toolbaritem;
             }
         });
     } catch(e) {}
 
-    var setPageTitle = function setPageTitle() {
-        if (document.getElementById('pagetitle-bar') === null) return;
-        var pageTitle = document.getElementById('main-window').getAttribute('title');
+    function setPageTitle () {
+        if (document.getElementById("pagetitle-bar") === null) {
+            return;
+        }
+        const pageTitleBar = document.getElementById("pagetitle-bar");
+
+        let pageTitle = document.getElementById("main-window").getAttribute("title");
         //remove the - Mozilla Firefox at the end of the title, delete the below line if you don't want it
         pageTitle = pageTitle.replace(/ - Mozilla Firefox$/, "");
         //remove the - Mozilla Firefox (Private Browsing) in the title, delete the below line if you don't want it
         pageTitle = pageTitle.replace(" - Mozilla Firefox (Private Browsing)", "");
-        document.getElementById('pagetitle-bar').setAttribute('titlepage', pageTitle);
+        pageTitleBar.setAttribute("titlepage", pageTitle);
+        pageTitleBar.setAttribute("tooltiptext", pageTitle);
     };
-    var observer = new MutationObserver(setPageTitle);
-    observer.observe(document.getElementById('main-window'), { attributes: true, attributeFilter: ["title"] });
+    const observer = new MutationObserver(setPageTitle);
+    observer.observe(document.getElementById("main-window"), {
+        attributes: true,
+        attributeFilter: ["title"]
+    });
 
+    const css = `
 
-    Cu.import('resource://gre/modules/Services.jsm');
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var css = '';
-    css += '@-moz-document url("chrome://browser/content/browser.xul") {';
-    css += ':root {';
-    css += '  --pagetitle-bar-width: 250px;'; //Change this value if you want shorter/longer bar
-    css += '}';
-    css += '#main-window:not([customizing]) #pagetitle-bar {';
-    css += '  -moz-window-dragging: drag;'; //Remove this line if you dont want it to be dragable
-    css += '  width: var(--pagetitle-bar-width);';
-    css += '  margin-bottom: 2px;'; //Change this value if you find the text appears too low or too high
-    css += '  overflow: hidden;';
-    css += '  text-overflow: "..";';
-    css += '  white-space: nowrap;';
-    css += '}';
-    css += '#main-window:not([customizing]) #pagetitle-bar::after {';
-    css += '  content: attr(titlepage);';
-    css += '  margin-left: 10px;';
-    css += '  margin-right: 10px;';
-    css += '}';
-    css += '#main-window:not([customizing]) #pagetitle-bar-image {';
-    css += '  display:none;';
-    css += '}';
-    css += '#main-window[customizing] #pagetitle-bar {';
-    css += '  margin: 12px;';
-    css += '}';
-    css += '#main-window[customizing] #nav-bar #pagetitle-bar {';
-    css += '  width: var(--pagetitle-bar-width);';
-    css += '  margin: 0 0 2px 0;';
-    css += '}';
-    css += '#main-window[customizing] #nav-bar #pagetitle-bar-image {';
-    css += '  display:none;';
-    css += '}';
-    css += '#main-window[customizing] #nav-bar #pagetitle-bar:after {';
-    css += '  content: "Page Title Bar";';
-    css += '  margin-left: 10px;';
-    css += '  margin-right: 10px;';
-    css += '}';
-    var cssEnc = encodeURIComponent(css);
-    var newURIParam = {
-        aURL: 'data:text/css,' + cssEnc,
-        aOriginCharset: null,
-        aBaseURI: null
-    };
-    var cssUri = Services.io.newURI(newURIParam.aURL, newURIParam.aOriginCharset, newURIParam.aBaseURI);
-    sss.loadAndRegisterSheet(cssUri, sss.AUTHOR_SHEET);
-})();
+/* Movable titlebar */
+
+:root {
+--pagetitle-bar-width: 350px;
+}
+
+#main-window:not([customizing]) #pagetitle-bar {
+-moz-window-dragging: drag;
+background: transparent;
+width: var(--pagetitle-bar-width);
+overflow: hidden;
+text-overflow: ellipsis;
+white-space: nowrap;
+margin: 0 10px;
+}
+
+#main-window:not([customizing]) #pagetitle-bar::after {
+-moz-window-dragging: no-drag;
+content: attr(titlepage);
+}
+
+#main-window:not([customizing]) #pagetitle-bar-image {
+display:none;
+}
+
+toolbarpaletteitem[place="palette"] > #pagetitle-bar {
+width: 7em;
+min-width: 7em;
+outline: 1px solid;
+outline-offset: -8px;
+opacity: .6;
+height: 37px;
+}
+
+#main-window[customizing] #nav-bar #pagetitle-bar {
+width: var(--pagetitle-bar-width);
+margin: 0 10px;
+}
+
+#main-window[customizing] #nav-bar #pagetitle-bar-image {
+display:none;
+}
+
+#main-window[customizing] #nav-bar #pagetitle-bar::after {
+content: "Page Title Bar";
+}`;
+
+    const sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+    const uri = makeURI("data:text/css;charset=UTF=8," + encodeURIComponent(css));
+    sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+}());
