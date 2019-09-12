c5d9faf60c8665747ba1295b0f03d2600c317f0a
Quang Trinh <aleksander.trinhquang@gmail.com>
2019-09-12 23:30:43 +0700

    Update for FF69

--- dblclickDuplicateTab.uc.js
+++ dblclickDuplicateTab.uc.js
@@ -1,12 +1,23 @@
-(function() {
-    gBrowser.tabContainer.addEventListener("dblclick", function(event) {
-        if (event.target.localName != "tab" && event.button != 0) {
-            return;
-        }
-        //Duplicate tab then move to that new tab directly
-        gBrowser.selectedTab = gBrowser.duplicateTab(event.target);
+(function () {
+  gBrowser.tabContainer.addEventListener("dblclick", function (event) {
+    if (event.target.localName !== "tab" && event.button !== 0) {
+      return;
+    }
+    const tab = document.evaluate(
+      'ancestor-or-self::*[local-name()="tab"]',
+      event.originalTarget,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    ).singleNodeValue;
+    if (!tab) {
+      return;
+    }
 
-        //Duplicate tab and load it in background
-        //gBrowser.duplicateTab(event.target).reload();
-    });
+    // Duplicate tab then move to that new tab directly
+    gBrowser.selectedTab = gBrowser.duplicateTab(tab);
+
+    // Duplicate tab and load it in background
+    // gBrowser.duplicateTab(tab);
+  });
 })();
