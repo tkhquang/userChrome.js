67db44bc07230499cb819ceed312a3377354c6a3
Quang Trinh <khacquang.trinh@gmail.com>
2019-09-12 23:53:46 +0700

    Minor update

--- TST_tabbar.uc.js
+++ TST_tabbar.uc.js
@@ -1,20 +1,20 @@
-//Auto hide tab bar only when Tree Style Tab sidebar is selected and visible.
+// Auto hide tab bar only when Tree Style Tab sidebar is selected and visible.
 
 (function () {
-    "use strict";
+  "use strict";
 
-    const tabbar = document.getElementById("TabsToolbar");
-    function showHideTabbar() {
-        const sidebarBox = document.getElementById("sidebar-box");
-        const sidebarCommand = sidebarBox.getAttribute("sidebarcommand");
-        tabbar.style.visibility = (
-            (!sidebarBox.hidden && sidebarCommand === "treestyletab_piro_sakura_ne_jp-sidebar-action") ? "collapse" : "visible");
-    }
-    const observer = new MutationObserver(showHideTabbar);
-    observer.observe(document.getElementById("sidebar-box"), {
-        attributes: true,
-        attributeFilter: [
-            "sidebarcommand",
-            "hidden"]
-    });
+  const tabbar = document.getElementById("TabsToolbar");
+  const sidebarBox = document.getElementById("sidebar-box");
+  function showHideTabbar() {
+    const sidebarCommand = sidebarBox.getAttribute("sidebarcommand");
+    tabbar.style.visibility = (
+      (!sidebarBox.hidden && sidebarCommand === "treestyletab_piro_sakura_ne_jp-sidebar-action") ? "collapse" : "visible");
+  }
+  const observer = new MutationObserver(showHideTabbar);
+  observer.observe(sidebarBox, {
+    attributes: true,
+    attributeFilter: [
+      "sidebarcommand",
+      "hidden"]
+  });
 }());
