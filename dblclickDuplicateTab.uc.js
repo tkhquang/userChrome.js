631c7cac7800a20ee2d36ab39bed38c3ad9d7604
Quang Trinh <khacquang.trinh@gmail.com>
2020-03-23 21:44:35 +0700

    Update for Firefox 75

--- dblclickDuplicateTab.uc.js
+++ dblclickDuplicateTab.uc.js
@@ -1,23 +1,37 @@
 (function () {
-  gBrowser.tabContainer.addEventListener("dblclick", function (event) {
-    if (event.target.localName !== "tab" && event.button !== 0) {
-      return;
-    }
-    const tab = document.evaluate(
-      'ancestor-or-self::*[local-name()="tab"]',
-      event.originalTarget,
-      null,
-      XPathResult.FIRST_ORDERED_NODE_TYPE,
-      null
-    ).singleNodeValue;
-    if (!tab) {
-      return;
-    }
+  const initilize = () => {
+    try {
+      gBrowser.tabContainer.addEventListener("dblclick", function ({
+        target: { localName },
+        button,
+        originalTarget,
+      }) {
 
-    // Duplicate tab then move to that new tab directly
-    gBrowser.selectedTab = gBrowser.duplicateTab(tab);
+        if (localName !== "tab" && button !== 0) {
+          return;
+        }
 
-    // Duplicate tab and load it in background
-    // gBrowser.duplicateTab(tab);
-  });
+        const tab = document.evaluate(
+          'ancestor-or-self::*[local-name()="tab"]',
+          originalTarget,
+          null,
+          XPathResult.FIRST_ORDERED_NODE_TYPE,
+          null
+        ).singleNodeValue;
+        if (!tab) {
+          return;
+        }
+
+        // Duplicate tab then move to that new tab directly
+        gBrowser.selectedTab = gBrowser.duplicateTab(tab);
+
+        // Duplicate tab and load it in background
+        // gBrowser.duplicateTab(tab);
+      });
+    } catch (e) {}
+  };
+
+  setTimeout(() => {
+    initilize();
+  }, 500);
 })();
